89100c03f23e041d35b3da591e41f940de677b63
Bibo Boom <bibokim6@gmail.com>
2023-02-19 10:36:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import Card from './components/Card';
+import SignUp from './pages/signup/SignUp';
+
+function App() {
+  return (
+    <div>   
+      <SignUp />
+      {/* <Card /> */}
+    </div>
+  );
+}
+
+export default App;
